084d7842fac7bf1f6f5d1a7f78cb09f82bfaaf01
M.C. van den Boogaart <scola@kvdb.name>
2016-09-08 22:53:20 +0200

    Add import for side-effects

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import { event, select } from 'd3-selection';
 import { slider } from '@scola/d3-slider';
+import '@scola/d3-gesture';
+import '@scola/d3-media';
 
 export default class App {
   constructor() {
